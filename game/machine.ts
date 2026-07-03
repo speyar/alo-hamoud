af58a151f86edf1e8122d2ca3f964182cced72b4
Karim <127413175+pywolf1503@users.noreply.github.com>
2026-07-03 11:41:37 +0100

    default config

--- game/machine.ts
+++ game/machine.ts
@@ -2,9 +2,13 @@ import { setup } from "xstate";
 import { GameEvent, type GameContext } from "./types";
 
 // machine setup for type safety
-const machineSetp = setup({
+const machineSetup = setup({
   types: {
     context: {} as GameContext,
     events: {} as GameEvent,
   },
 });
+
+
+
+
